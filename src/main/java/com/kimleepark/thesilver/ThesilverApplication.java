25fac436bed834abfd22bd8405eca027a1be30e8
kingnongmeel <kymtlfgdh7@naver.com>
2023-11-21 17:59:28 +0900

    231121-thesilver-템플릿

---
+++ src/main/java/com/kimleepark/thesilver/ThesilverApplication.java
@@ -0,0 +1,13 @@
+package com.kimleepark.thesilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesilverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesilverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimleepark/thesilver/ThesilverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimleepark.thesilver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesilverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
