efac5b80df27d757b8c4ec2b7de782ec5a65cc6b
kingnongmeel <kymtlfgdh7@naver.com>
2023-11-22 12:08:56 +0900

    231121-thesilver-템플릿

--- src/test/java/com/kimleepark/thesilver/ThesilverApplicationTests.java
+++ src/test/java/com/kimleepark/thesilver/ThesilverApplicationTests.java
@@ -1,13 +1,35 @@
 package com.kimleepark.thesilver;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class ThesilverApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+
     }
 
 }
